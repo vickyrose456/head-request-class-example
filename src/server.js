3a4437e1c048432649d0c5d4433172e6423e4477
Victoria Olivieri <70556187+vickyrose456@users.noreply.github.com>
2022-02-03 17:09:16 -0500

    Updated code from halfway through video.

--- src/server.js
+++ src/server.js
@@ -5,10 +5,34 @@ const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
+const urlStruct = {
+  GET: {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getCSS,
+    '/getUsers': jsonHandler.getUsers,
+    '/updateUser': jsonHandler.updateUser,
+    notFound: jsonHandler.notFound,
+  },
+  HEAD: {
+    '/getUsers': jsonHandler.getUsersMeta,
+    // need to put the meta one b/c it sends back a message and header
+    notFound: jsonHandler.notFoundMeta,
+  },
+};
+
 const onRequest = (request, response) => {
   const parsedUrl = url.parse(request.url);
 
+  console.dir(parsedUrl.pathname);
+  console.dir(request.method);
 
+  // if it exist, then index in by parsed url
+  if (urlStruct[request.method][parsedUrl.pathname]) {
+    urlStruct[request.method][parsedUrl.pathname](request, response);
+  } else {
+    // url does not exist
+    urlStruct[request.method].notFound(request, response);
+  }
 };
 
 http.createServer(onRequest).listen(port, () => {
